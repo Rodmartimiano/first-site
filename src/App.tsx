5dbafe037a82dd64c949558b5ac431f46368c1a5
Rodrigo Martimiano <rodrigo@DESKTOP-97PGBGG>
2024-09-23 20:01:48 -0300

    Features section

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 import HeaderApp from "./components/Header/HeaderApp";
 import Hero from "./components/Hero/Hero";
+import Features from "./components/Features/Features";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <HeaderApp />
       </div>
       <Hero />
+      <Features />
     </div>
   );
 }
---
+++ src/components/Features/Features.tsx
@@ -0,0 +1,87 @@
+import "./Features.css";
+import cellphone from "../../assets/cellphone.png";
+import badgeInstant from "../../assets/badgeInstant.svg";
+import badgeSaving from "../../assets/badgeSaving.svg"; // Additional icons for other features
+import badgeMobile from "../../assets/badgeMobile.svg";
+import badgestatistics from "../../assets/badgestatistics.svg";
+import badgeCard from "../../assets/badgeCard.svg";
+import badgeContactless from "../../assets/badgeContactless.svg";
+
+function Features() {
+  return (
+    <div className="Features">
+      <div className="container">
+        <div className="left">
+          <h1 className="header-medium">One app. One banking.</h1>
+          <div className="list">
+            <div className="features-text-item">
+              <img src={badgeInstant} alt="Instant transactions" />
+              <div>
+                <h3>Saving Accounts</h3>
+                <p>
+                  Odio euismod lacinia at quis. Amet purus gravida quis blandit
+                  turpis.
+                </p>
+              </div>
+            </div>
+            <div className="features-text-item">
+              <img src={badgeSaving} alt="saving badge" />
+              <div>
+                <h3>Saving Accounts</h3>
+                <p>
+                  Odio euismod lacinia at quis. Amet purus gravida quis blandit
+                  turpis.
+                </p>
+              </div>
+            </div>
+            <div className="features-text-item">
+              <img src={badgeMobile} alt="mobile banking badge" />
+              <div>
+                <h3>Mobile Banking</h3>
+                <p>
+                  Odio euismod lacinia at quis. Amet purus gravida quis blandit
+                  turpis.
+                </p>
+              </div>
+            </div>
+            <div className="features-text-item">
+              <img src={badgestatistics} alt="Advanced statistics" />
+              <div>
+                <h3>Virtual Cards</h3>
+                <p>
+                  Odio euismod lacinia at quis. Amet purus gravida quis blandit
+                  turpis.
+                </p>
+              </div>
+            </div>
+            <div className="features-text-item">
+              <img src={badgeCard} alt="virtual card badge" />
+              <div>
+                <h3>Virtual Cards</h3>
+                <p>
+                  Odio euismod lacinia at quis. Amet purus gravida quis blandit
+                  turpis.
+                </p>
+              </div>
+            </div>
+            <div className="features-text-item">
+              <img src={badgeContactless} alt="Contactless payments" />
+              <div>
+                <h3>Virtual Cards</h3>
+                <p>
+                  Odio euismod lacinia at quis. Amet purus gravida quis blandit
+                  turpis.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="cellphone">
+          <img src={cellphone} alt="Cellphone" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Features;
