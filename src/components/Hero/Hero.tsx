25ee1bec4c9dbac75c1e973178836c9a7be357bb
Gio <tec@seescontabilidade.com.br>
2024-09-23 18:50:12 -0300

    Added initial structure fro dynamic data for Hero template

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,50 +3,92 @@ import "./Hero.css";
 import Label from "../Label/Label";
 import cardimg from "../../assets/cards.svg";
 import badge from "../../assets/badge.svg";
+import { useEffect, useState } from "react";
+
+type HeroData = {
+  mainTitle: string;
+  description: string;
+  badgeTexts: string[];
+  showOpenAccount: boolean;
+  showCompareCards: boolean;
+};
+
+const heroMock: HeroData = {
+  mainTitle: "",
+  description: "",
+  badgeTexts: [],
+  showOpenAccount: false,
+  showCompareCards: false,
+};
+
+function getServerData(): HeroData {
+  return {
+    mainTitle: "Banking starts here",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.",
+    badgeTexts: [
+      "Instant Transfer",
+      "Easy to use",
+      "Access globally",
+      "Online support",
+    ],
+    showOpenAccount: true,
+    showCompareCards: true,
+  };
+}
 
 function Hero() {
+  const [heroJsonData, setHeroJsonData] = useState<HeroData>(heroMock);
+
+  useEffect(() => {
+    const serverData = getServerData();
+
+    setHeroJsonData(serverData);
+  }, []);
+
   return (
     <div className="Hero">
       <div className="container">
         <div className="grid">
           <div className="title">
-            <h1 className="header-medium">Banking starts here.</h1>
+            <h1 className="header-medium">{heroJsonData.mainTitle}</h1>
           </div>
+
           <div className="description">
-            <h2>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore.
-            </h2>
+            <h2>{heroJsonData.description}</h2>
           </div>
+
           <div className="list">
-            <div className="hero-text-item">
-              <img src={badge} alt="badge images" />
-              <span>Instant Transfer</span>
-            </div>
-            <div className="hero-text-item">
-              <img src={badge} alt="badge images" />
-              <span>Instant Transfer</span>
-            </div>
-            <div className="hero-text-item">
-              <img src={badge} alt="badge images" />
-              <span>Instant Transfer</span>
-            </div>
-            <div className="hero-text-item">
-              <img src={badge} alt="badge images" />
-              <span>Instant Transfer</span>
-            </div>
+            {heroJsonData.badgeTexts.map((badgeText) => {
+              return (
+                <div className="hero-text-item">
+                  <img src={badge} alt="badge images" />
+                  <span>{badgeText}</span>
+                </div>
+              );
+            })}
           </div>
+
           <div className="bottom-buttons">
-            <BasicButton
-              label="Open Account"
-              size="medium"
-              appearence="secondary"
-            />
-            <Label
-              label="Compare Cards ->"
-              size="medium"
-              appearence="secondary"
-            />
+            {heroJsonData.showOpenAccount ? (
+              <BasicButton
+                label="Open Account"
+                size="medium"
+                appearence="secondary"
+              />
+            ) : (
+              ""
+            )}
+
+            {heroJsonData.showCompareCards ? (
+              <Label
+                label="Compare Cards ->"
+                size="medium"
+                appearence="secondary"
+              />
+            ) : (
+              ""
+            )}
           </div>
         </div>
         <div className="cards">
